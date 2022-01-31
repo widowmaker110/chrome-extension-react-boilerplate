08532b87a5675a387e2151ea9b1f73b6be04870f
Alex <alexander.miller110@gmail.com>
2022-01-30 20:56:33 -0500

    update to read current tabs

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -1,5 +1,7 @@
 // Replace your background.js code here
 
+const api_endpoint = 'https://0ahl740628.execute-api.us-east-2.amazonaws.com';
+
 chrome.runtime.onInstalled.addListener(function (details) {
     if (details.reason == "install") {
         console.log("This is a first install!");
@@ -7,4 +9,13 @@ chrome.runtime.onInstalled.addListener(function (details) {
         var thisVersion = chrome.runtime.getManifest().version;
         console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
     }
+});
+
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+    console.log(tabs[0].url);
+
+    fetch(api_endpoint + '/prod/addresses').then(r => r.text()).then(result => {
+        // Result now contains the response text, do what you want...
+        console.log(result);
+    });
 });
